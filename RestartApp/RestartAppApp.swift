3f1cc8f247782a4bcdb1de537ceb3dda0671fd66
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-06-20 14:28:40 -0500

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Justin Hold on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
